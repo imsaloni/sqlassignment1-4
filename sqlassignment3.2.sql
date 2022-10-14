0f02a63c0b0b3e7257e8ff9cf1d96592b56a9752
imsaloni <115076951+imsaloni@users.noreply.github.com>
2022-10-14 06:51:34 +0530

    Add files via upload

---
+++ sqlassignment3.2.sql
@@ -0,0 +1,4 @@
+insert into Shipping values (1,'abc',21/03/2018,'dtgf')
+insert into Shipping values (2,'dhj',23/03/2018,'pfg')
+insert into Shipping values (3,'dfgh',25/03/2018,'pqr')
+insert into Shipping values (4,'hfxcd',26/03/2018,'dfr')
