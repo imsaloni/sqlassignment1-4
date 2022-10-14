1c532477fc73a29ead30444bd948f2f8f00fd044
imsaloni <115076951+imsaloni@users.noreply.github.com>
2022-10-14 06:52:32 +0530

    Add files via upload

---
+++ sqlassignment4.sql
@@ -0,0 +1,10 @@
+select * from Ordertbl
+select * from Product1
+select * from OrderItem
+select ProductName,UnitPrice From Product1 ORDER BY UnitPrice DESC;
+select * from Ordertbl Where CustomerId=1
+select * from Product1 Where ProductSuppliedby<Package
+select CustomerId ,OrderNumber from Ordertbl
+Alter Table Product ADD OrderDate Date
+select * from Employee 
+select * from Employee Where Rating=4
