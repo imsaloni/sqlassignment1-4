7e61099061e32f6fe93751d3599faf47cd104af3
imsaloni <115076951+imsaloni@users.noreply.github.com>
2022-10-14 06:49:00 +0530

    Add files via upload

---
+++ sqlassignment1.2.sql
@@ -0,0 +1,4 @@
+Insert into Ordertbl(ID,OrderDate,OrderNumber,CustomerId,TotalAmount) values(501,22-08-2001,234,1,1219.8)
+Insert into Ordertbl(ID,OrderDate,OrderNumber,CustomerId,TotalAmount) values(502,08-03-2016,435,2,2269.6)
+Insert into Ordertbl(ID,OrderDate,OrderNumber,CustomerId,TotalAmount) values(503,19-07-2011,234,3,3559.7)
+
