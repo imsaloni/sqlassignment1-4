ee0bf5d212a995fae357157267cf8a89193ea69e
imsaloni <115076951+imsaloni@users.noreply.github.com>
2022-10-14 06:48:38 +0530

    Add files via upload

---
+++ sqlassignment1.1.sql
@@ -0,0 +1,4 @@
+insert into customer  values (1,'Mathew','Peralt','Seattle','US',6112239986)
+insert into customer  values (2,'rocky','drew','Toronto','Cannada',8112239987)
+insert into customer  values (3,'Devid','stone','berline','Germany',9112239987)
+insert into customer  values (4,'Disha','deas','Mumbai','India',9112239989)
