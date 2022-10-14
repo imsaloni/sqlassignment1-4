6b08b778f99482ac1b0fbd452792b66f7b9f3396
imsaloni <115076951+imsaloni@users.noreply.github.com>
2022-10-14 11:53:53 +0530

    Update sqlassignment2.sql

--- sqlassignment2.sql
+++ sqlassignment2.sql
@@ -5,5 +5,6 @@ select Name,Department From Employee
 select * from Ordertbl
 select * from OrderItem where UnitPrice/1000 >10 AND UnitPrice/1000 < 20
 select * from Ordertbl ORDER BY OrderDate
+Select * From Ordertbl Where CompanyName='Exotic Liquid'
 select * from Ordertbl Where OrderNumber=3
 select Quantity*UnitPrice as TOTAL from OrderItem
