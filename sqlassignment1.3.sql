3f4c5403aba772c940abf85261507ef457b4cdea
imsaloni <115076951+imsaloni@users.noreply.github.com>
2022-10-14 06:49:20 +0530

    Add files via upload

---
+++ sqlassignment1.3.sql
@@ -0,0 +1,5 @@
+
+Insert into Product1 values(401,'Pen',10,6,2,3)
+Insert into Product1 values(402,'Pencil',20,4,2,6)
+Insert into Product1 values(403,'Eraser',15,7,3,4)
+Insert into Product1 values(404,'Sharpner',60,8,1,2)
