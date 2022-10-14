9de5fbdc6db3f7894a50863253d7b69142404e23
imsaloni <115076951+imsaloni@users.noreply.github.com>
2022-10-14 11:50:44 +0530

    Update sqlassignment1.3.sql

--- sqlassignment1.3.sql
+++ sqlassignment1.3.sql
@@ -1,5 +1,7 @@
 
-Insert into Product1 values(401,'Pen',10,6,2,3)
-Insert into Product1 values(402,'Pencil',20,4,2,6)
-Insert into Product1 values(403,'Eraser',15,7,3,4)
-Insert into Product1 values(404,'Sharpner',60,8,1,2)
+Insert into Product1(ID,ProductName,UnitPrice,Package,IsDicountinue,ProductSuppliedBy) values(401,'Pen',10,6,2,3,Exostic Liquid)
+Insert into Product1(ID,ProductName,UnitPrice,Package,IsDicountinue)values(402,'Pencil',20,4,2,6)
+Insert into Product1(ID,ProductName,UnitPrice,Package,IsDicountinue) values(403,'Eraser',15,7,3,4)
+Insert into Product1(ID,ProductName,UnitPrice,Package,IsDicountinue) values(404,'Sharpner',60,8,1,2)
+Insert into Product1(ID,ProductName,UnitPrice,Package,IsDicountinue) values(405,'Book',100,4,7,9)
+
