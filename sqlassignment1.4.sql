afe9767ddcb281332a77297fbcc483ac4e4cf7a7
imsaloni <115076951+imsaloni@users.noreply.github.com>
2022-10-14 06:49:41 +0530

    Add files via upload

---
+++ sqlassignment1.4.sql
@@ -0,0 +1,3 @@
+Insert into OrderItem(ID,OrderId,ProductId,UnitPrice,Quantity) values(1,501,401,1232.9,3)
+Insert into OrderItem(ID,OrderId,ProductId,UnitPrice,Quantity) values(2,502,402,2211.7,7)
+Insert into OrderItem(ID,OrderId,ProductId,UnitPrice,Quantity) values(3,503,403,6453.0,5)
