f6724f8dad5f25922f5228941a67d838049f4b5c
imsaloni <115076951+imsaloni@users.noreply.github.com>
2022-10-14 06:51:10 +0530

    Add files via upload

---
+++ sqlassignment3.1.sql
@@ -0,0 +1,4 @@
+insert into Employee values (1,101,'sales','Aman',5)
+insert into Employee values (2,102,'marketing','rohan',4)
+insert into Employee values (3,103,'sales','riya',9)
+insert into Employee values (4,104,'it','ravi',8)
