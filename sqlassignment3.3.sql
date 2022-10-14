5293618e61f545cfeaf0133be840b8b041127df6
imsaloni <115076951+imsaloni@users.noreply.github.com>
2022-10-14 06:51:55 +0530

    Add files via upload

---
+++ sqlassignment3.3.sql
@@ -0,0 +1,7 @@
+select * from Customer
+select * from Customer Where Phone=6112239986
+select * from Customer Where City='mumbai'
+select * from Customer Where Country='India'
+select * from Product1 Where ProductName='samosha'
+select * from Employee
+select * from Employee Where Id=2
